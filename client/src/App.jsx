3dd70a067a8a3a70da021774bdf926c33551866e
kooroshFarv <farvardin.koorosh@gmail.com>
2026-02-11 12:58:33 -0500

    first set up front and back + db + jwt

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+<div className="min-h-screen w-full flex items-center justify-center bg-slate-900">
+      <div className="text-4xl font-bold text-red-700">
+        HEY THEREEEEE
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ server/src/server.js
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import mongoose from "mongoose";
+import { Server } from "socket.io";
+
+const app = express();
+
+app.use(cors({origin: 'http://localhost:5173'}));
+app.use(express.json());
+app.get('/', (req, res) => res.send('running'))
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {origin: 'http://localhost:5173'}
+})
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+    socket.on('disconnect', () => {
+        console.log(socket.id)
+    })
+})
+
+try {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log('DB connected')
+    const PORT = process.env.PORT || 3001;
+    server.listen(PORT, () => console.log(`server running on ${PORT}`))
+} catch (err) {
+    console.error('DB error', err.message)
+    process.exit(1)
+}
